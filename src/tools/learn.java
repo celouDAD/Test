db55a254a1f1bdc65795bb4393dba52af628e020
celouDAD <511248665@qq.com>
2020-07-24 18:35:31 +0800

    update

--- src/tools/learn.java
+++ src/tools/learn.java
@@ -2,19 +2,19 @@ package tools;
 
 public class learn {
 	public static void main(String[] args) {
-		 int i = 0;
-	        switch(i){
-	        case 0:
-	            System.out.println("0");
-	            break;
-	        case 1:
-	            System.out.println("1");
+		int i = 0;
+		switch (i) {
+		case 0:
+			System.out.println("0");
+			break;
+		case 1:
+			System.out.println("1");
 		case 2:
-	            System.out.println("2");
-	        default:
-	            System.out.println("default");
-	        }
-		
+			System.out.println("2");
+		default:
+			System.out.println("default");
+		}
+
 	}
 
 }
--- src/tools/print.java
+++ src/tools/print.java
@@ -4,7 +4,10 @@ public class print {
 	public void print(Object obj) {
 		System.out.print(obj);
 	}
+
 	public void println(Object obj) {
 		System.out.println(obj);
+
 	}
+
 }
